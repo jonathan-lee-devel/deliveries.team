fed8e4d2a12f21b738adbe23a8a7006eb1b8d993
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-09-10 01:28:11 +0100

    Removed reference to non-existent route

--- services/deliveries-team-ui/src/routes/+page.svelte
+++ services/deliveries-team-ui/src/routes/+page.svelte
@@ -19,7 +19,7 @@
     <Button color="primary">Primary</Button>
 </div>
 <div class="w-full flex justify-center items-center pt-5">
-    <Card href="/cards" color="primary">
+    <Card href="/" color="primary">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">Noteworthy technology acquisitions 2021</h5>
         <p class="font-normal text-gray-700 dark:text-gray-700 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
     </Card>
