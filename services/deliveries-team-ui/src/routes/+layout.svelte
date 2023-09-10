628c08fa7e2d2ca20302996e7b215db7f940086a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-09-10 01:47:33 +0100

    Updated favicon

--- services/deliveries-team-ui/src/routes/+layout.svelte
+++ services/deliveries-team-ui/src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <Navbar let:hidden let:toggle color="primary">
     <NavBrand href="/">
-    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <img src="favicon.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
     <span class="self-center whitespace-nowrap text-xl font-semibold text-white ">Deliveries.Team</span>
 </NavBrand>
 <NavHamburger on:click={toggle} />
